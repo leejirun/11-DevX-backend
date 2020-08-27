a0d7a05a0b975808cf3959cbfb53612205accbfa
leejirun <wldus9503@gmail.com>
2020-08-27 13:47:30 +0900

    [이지연]modified:algorithm part

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls    import path
 
-from .views         import SignUpView,SignInView
+from .views         import SignUpView
+from .views         import SignInView
 
 urlpatterns = [
     path('/signup',SignUpView.as_view()),
