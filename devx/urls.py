19de46f7db9b5e11ee27ef5970cf6bfb00927d06
leejirun <wldus9503@gmail.com>
2020-08-28 12:08:07 +0900

    [이지연]Modified:  conflict problem - devx.urls

--- devx/urls.py
+++ devx/urls.py
@@ -1,11 +1,7 @@
 from django.urls        import path,include
 
-from .views             import ProductsView, ProductView
-
 urlpatterns = [
     path('user', include('user.urls')),
-    path('', ProductsView.as_view()),
-    path('/<int:id>', ProductView.as_view()),
-    path('order', include('order.urls'))
+    path('products', include('product.urls')),
+    Path('order', include('order.urls'))
 ]
-
